6d7d4a60992f88c87102e16039caac6b0e3727fc
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2025-05-30 22:50:13 +0000

    Reverse  Array added

--- Arrays/reverseArray.js
+++ Arrays/reverseArray.js
@@ -4,3 +4,16 @@
 // Example: reverseArray([1, 2, 3, 4]) should return [4, 3, 2, 1].
 
 // Hint: You can either use a loop to reverse it manually or utilize JavaScript’s built -in array methods.
+
+const  array  = [1, 2, 3, 4];
+
+function reverseArray(arr){
+    const reverseArray = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reverseArray.push(arr[i]);
+    }
+
+    return reverseArray;
+
+}
+console.log(reverseArray(array)); // [4, 3, 2, 1]
