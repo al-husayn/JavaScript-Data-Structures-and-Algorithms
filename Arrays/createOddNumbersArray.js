98529e307afd1fa9c7e87aa161c14b9f48d231f0
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2025-05-31 23:22:08 +0000

    updated

--- Arrays/createOddNumbersArray.js
+++ Arrays/createOddNumbersArray.js
@@ -4,9 +4,9 @@
 
 // Implement a function createOddNumbersArray(n) that takes a positive integer n and returns an array containing the first n odd numbers.
 
-//     Example: createOddNumbersArray(5) should return [1, 3, 5, 7, 9].
+//    Example: createOddNumbersArray(5) should return [1, 3, 5, 7, 9].
 
-//         Hint: Start from 1 and keep adding 2 until you have n elements.
+//    Hint: Start from 1 and keep adding 2 until you have n elements.
 
 
 function createOddNumbersArray(n) {
