b03644a51bf30f771725fc26522d21b51b6e0934
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2025-06-01 22:05:15 +0000

    comments added

--- Arrays/sumArray.js
+++ Arrays/sumArray.js
@@ -9,12 +9,19 @@
 
 const array = [5, 10, 15]
 
+// This function adds up all the numbers in the array and gives us the total
+
 // function sumArray(arr){
 //     return arr.reduce((sum, num)=>sum + num, 0)
 
 // }
 const sumArray = (arr) => {
+    // We use reduce to go through each number in the array
+    // sum starts at 0, and for each number (num), we add it to sum
+    // At the end, we get the total sum of all numbers
     return arr.reduce((sum, num) => sum + num, 0)
 }
 
+// Let's see what happens when we use our function!
+// This will print 30 because 5 + 10 + 15 = 30
 console.log(sumArray(array))// Output: 30
